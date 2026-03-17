8a2104a36c9d8f167f8d972fb2aa4673655a4d7d
Lucas Cowden <lucas.cowden@us.af.mil>
2026-03-16 23:19:28 -0400

    Edited code

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -36,7 +36,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     
     const indexOfLastExercise = currentPage * exercisesPerPage;
     const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-    const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+    const currentExercises = (exercises || []).slice(indexOfFirstExercise, indexOfLastExercise);
   
     return (
     <Box id="exercises"
